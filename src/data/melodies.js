15fa452347ab9936f3f8b7cae73ce58669b0e315
leaf corcoran <leafot@gmail.com>
2026-01-12 15:25:39 -0800

    clean up all the melodies, gotta fix them

--- src/data/melodies.js
+++ src/data/melodies.js
@@ -1,34 +1,11 @@
 import { lml } from './lmlParser.js';
 
 // Import LML files as raw text (esbuild text loader)
-import maryHadALittleLamb from './melodies/mary-had-a-little-lamb.lml';
 import amazingGraceLml from './melodies/amazing-grace.lml';
-import ohSusannaLml from './melodies/oh-susanna.lml';
-import homeOnTheRangeLml from './melodies/home-on-the-range.lml';
-import simpleGiftsLml from './melodies/simple-gifts.lml';
-import twinkleTwinkleLml from './melodies/twinkle-twinkle.lml';
-import shenandoahLml from './melodies/shenandoah.lml';
-import jingleBellsLml from './melodies/jingle-bells.lml';
-import silentNightLml from './melodies/silent-night.lml';
-import weWishYouAMerryChristmasLml from './melodies/we-wish-you-a-merry-christmas.lml';
-import joyToTheWorldLml from './melodies/joy-to-the-world.lml';
 
 // Parse LML files into melody format
 // The parsed format is compatible with the existing playback system:
 // { name: string, category: string, tempo: number, notes: [[degree, octave, beats], ...] }
 export const melodies = [
-    lml(maryHadALittleLamb),
-    // Traditional American
     lml(amazingGraceLml),
-    lml(ohSusannaLml),
-    lml(homeOnTheRangeLml),
-    lml(simpleGiftsLml),
-    lml(twinkleTwinkleLml),
-    lml(shenandoahLml),
-
-    // Christmas
-    lml(jingleBellsLml),
-    lml(silentNightLml),
-    lml(weWishYouAMerryChristmasLml),
-    lml(joyToTheWorldLml),
 ];
